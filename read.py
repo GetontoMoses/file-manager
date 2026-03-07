a9519fa41d61249b4d6aaf03164894fb30b563d4
Getonto3 <getontomoses18@gmail.com>
2026-03-08 00:37:50 +0300

    feat: writing into a file

--- read.py
+++ read.py
@@ -1,5 +1,5 @@
 # opening
-with open("file.txt", "r", encoding="utf-16") as file:
+with open("file.txt", "r", encoding="utf-16") as file:# using r it overwrites the existing code
 
     # read
     content = file.read()
