51340a865f6b7621e5400cd6381467ff6178541a
Getonto3 <getontomoses18@gmail.com>
2026-03-08 01:05:35 +0300

    fix: used readlines function to read each line

--- read.py
+++ read.py
@@ -2,7 +2,7 @@
 with open("file.txt", "r", encoding="utf-16") as file:# using r it overwrites the existing code
 
     # read
-    content = file.read()
+    content = file.readlines()
     
     # display
     print(content)
