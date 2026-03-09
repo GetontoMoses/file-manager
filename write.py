62699d3db459ccaa2acbd65b40f04c1c5afb13ac
Getonto3 <getontomoses18@gmail.com>
2026-03-09 23:16:55 +0300

    feat: deleting a file

--- write.py
+++ write.py
@@ -1,2 +1,2 @@
-with open("file.txt","w",encoding = "utf-16") as file:
-    new_text = file.write(input("Enter your name: "))
+with open("file.txt","a",encoding = "utf-16") as file:
+    new_text = file.write(input("Enter your name:"))
